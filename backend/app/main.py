334c85581e00c52ed7bc75dd9f55a6da3a2e888e
julsCadenas <jscadenas12@gmail.com>
2025-09-27 01:40:30 +0800

    feat: cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import router as api_router
 
 app = FastAPI(
     title="Superhero Chatbot API",
     description="A conversational AI powered by Gemini 2.5-Flash, Chroma, and HF embeddings",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix="/api")
